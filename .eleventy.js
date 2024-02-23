990f746fc395cc1c1a3f71569bec83b7ac36129a
Hcoleyh <hcoleyh@gmail.com>
2024-02-23 17:26:57 +0800

    Add: atom feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addFilter("getAttr", function (obj, attr) { return obj[attr]; });
+    eleventyConfig.addPlugin(pluginRss);
 };
